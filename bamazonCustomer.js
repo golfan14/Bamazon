34230637e3426b1f7338056ccff7efaabddbee43
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-05-30 19:15:45 -0600

    start function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,37 @@
-var mysql = require("myspl");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 8889,
-    username: "root",
+    user: "root",
     password: "root",
     database: "bamazon_db"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    connection.query("Select * From products", function (err, results) {
+        if (err) throw error;
+        var inventory = "";
+        for ( var i = 0; i < results.length; i++) {
+            inventory = "";
+            inventory += "Item ID: " + results[i].item_id + " // ";
+            inventory += "Product Name: " + results[i].product_name + " // ";
+            inventory += "Price: " + results[i].price + " // ";
+            inventory += "Stock Quantity: " + results[i].stock_quantity;
+
+            console.log(inventory);
+        };
+        
+        console.log("==================================================================================");
+        
+        promptPurchase();
+    
+    });
+};
 
